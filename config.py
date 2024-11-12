e387fbc4ba3e5225ea6426d2fe99c650216611a0
MAULANAadib <123590047+MAULANAadib@users.noreply.github.com>
2024-11-12 22:51:45 +0700

    config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Konfigurasi MySQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://pemweblanjut_basicscene:67e874a8660ad00268eccbcad526d1586c3cf748@wxqwb.h.filess.io:3306/pemweblanjut_basicscene'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://pemweblanjut_basicscene:67e874a8660ad00268eccbcad526d1586c3cf748@wxqwb.h.filess.io:3306/pemweblanjut_basicscene'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
